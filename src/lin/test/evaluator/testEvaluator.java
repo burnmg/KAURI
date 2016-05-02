297e55071c52263ad469466d057cbdfec98e1292
burnmg <rlin1992@hotmail.com>
2016-05-02 15:11:55 +1200

    c

---
+++ src/lin/test/evaluator/testEvaluator.java
@@ -0,0 +1,12 @@
+package lin.test.evaluator;
+
+import moa.tasks.EvaluatePrequential;
+
+public class testEvaluator {
+
+	public static void main(String[] args) {
+		EvaluatePrequential eva = new EvaluatePrequential();
+		
+	}
+
+}
